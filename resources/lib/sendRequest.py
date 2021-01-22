71190235206e824e40d27d9b6ba9d17c336fdec8
agarnele <76904352+agarnele@users.noreply.github.com>
2021-01-22 22:48:10 +0100

    Update sendRequest.py

--- resources/lib/sendRequest.py
+++ resources/lib/sendRequest.py
@@ -3,10 +3,10 @@
 import xbmc
 import xbmcaddon
 import urllib
-import urllib2
 import json
 import time
 import base64
+
 
 __addon__               = xbmcaddon.Addon()
 __lang__                = __addon__.getLocalizedString
@@ -23,7 +23,7 @@ def send(self, option, values=''):
     debug.debug('[REQUEST]: ' + str(values))
     
     # try send data
-    data = urllib.urlencode(values, True)
+    data = urllib.parse.urlencode(values, True)
     data_len = len(data)
     
     debug.debug('[REQUEST DATA SIZE]: ' + str(data_len) + ' bytes')
@@ -44,6 +44,36 @@ def send(self, option, values=''):
         except Exception as Error:
             conn = False
             debug.debug('Can\'t connect to: ' + self.setXBMC['URL'] + option)
+            debug.debug('[REQUEST ERROR]: ' + str(Error))
+            if l < 3:
+                debug.debug('[REQUEST]: Wait 5 secs and retring ' + str(l))
+            time.sleep(15)
+        else:
+            conn = True
+            break;
+        
+    if conn != True:
+        debug.notify(__lang__(32100).encode('utf-8'))
+        return False
+        
+    debug.debug('[RESPONSE]: ' + str(output))
+    
+    # if no values return json
+    if values == '':
+        try:
+            output = unicode(output, 'utf-8', errors='ignore')
+            output = json.loads(output)
+        except Exception as Error:
+            debug.debug('[GET JSON ERROR]: ' + str(Error))
+            return False
+    else:
+        #get errors
+        if len(output) > 0 and 'ERROR:' in output:
+            debug.notify(__lang__(32102).encode('utf-8'))
+            return False
+    
+    return output
+        
             debug.debug('[REQUEST ERROR]: ' + str(Error))
             if l < 3:
                 debug.debug('[REQUEST]: Wait 5 secs and retring ' + str(l))
