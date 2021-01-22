e3d313e361babc3f4bc066a1b33d5361656c3f00
agarnele <76904352+agarnele@users.noreply.github.com>
2021-01-22 22:50:29 +0100

    Update syncVideo.py

--- resources/lib/syncVideo.py
+++ resources/lib/syncVideo.py
@@ -119,3 +119,79 @@ def remove(self, videoToRemove, table):
         
     if removedCount > 0:
         debug.notify(__lang__(32105).encode('utf-8') + ' ' + __lang__(self.lang[table]).encode('utf-8') + ': ' + str(removedCount))
+
+                        
+                        # add hash to video array
+                        videoToUpdate[m] = videosXBMC[table][m]
+            debug.debug('[' + table + 'ToUpdate]: ' + str(videoToUpdate.keys()))
+            
+            # add videos
+            if len(videoToAdd) > 0:
+                debug.debug('=== ADDING ' + table.upper() + ' VIDEOS ===')
+                if add(self, videosXBMC, videoToAdd, table, 'add') is False:
+                    self.progBar.close()
+                    return False
+                self.progBar.close()
+                
+            # remove videos
+            if len(videoToRemove) > 0:
+                debug.debug('=== REMOVING ' + table.upper() + ' VIDEOS ===')
+                if remove(self, videoToRemove, table) is False:
+                    return False
+                
+            # update videos
+            if len(videoToUpdate) > 0:
+                debug.debug('=== UPDATING ' + table.upper() + ' VIDEOS ===')
+                if add(self, videosXBMC, videoToUpdate, table, 'update') is False:
+                    self.progBar.close()
+                    return False
+                self.progBar.close()
+                
+            # update hash
+            value = {table: hashlib.md5(str(videosXBMC[table])).hexdigest()}
+            sendRequest.send(self, 'updatehash', value)
+        
+def add(self, videosXBMC, videoToAdd, table, opt):
+    
+    # init progres bar
+    addedCount = 0
+    countToAdd = len(videoToAdd)
+    self.progBar.create(__lang__(32200), __addonname__ + ', ' + __lang__(32204 if opt == 'add' else 32209) + ' ' + __lang__(self.lang[table]))
+    
+    for video in videoToAdd:
+        start_time = time.time()
+        
+        # progress bar update
+        p = int((float(100) / float(countToAdd)) * float(addedCount))
+        progYear = ' (' + str(videosXBMC[table][video]['year']) + ')' if 'year' in videosXBMC[table][video] else ''
+        self.progBar.update(p, str(addedCount + 1) + '/' + str(countToAdd) + ' - ' + videosXBMC[table][video]['title'] + progYear)
+        
+        # get values
+        values = prepareValues.prep(self, videosXBMC[table][video], table)
+        
+        # send requst
+        if sendRequest.send(self, opt + 'video&t=' + table, values) is False:
+            return False
+        else:
+            addedCount += 1
+        debug.debug('[TIME]: ' + str(time.time() - start_time)[0:5])
+        
+    if addedCount > 0:
+        debug.notify(__lang__(32104 if opt == 'add' else 32103).encode('utf-8') + ' ' + __lang__(self.lang[table]).encode('utf-8') + ': ' + str(addedCount))
+
+def remove(self, videoToRemove, table):
+    
+    removedCount = 0
+    
+    # get values
+    values = {}
+    for video in videoToRemove:
+        removedCount += 1
+        values[removedCount] = video
+    
+    # send requst
+    if sendRequest.send(self, 'removevideo&t=' + table, values) is False:
+        return False
+        
+    if removedCount > 0:
+        debug.notify(__lang__(32105).encode('utf-8') + ' ' + __lang__(self.lang[table]).encode('utf-8') + ': ' + str(removedCount))
